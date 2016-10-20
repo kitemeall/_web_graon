c726c3ed925288e277c92bc827c361b2ffde3749
RostislavIvanov <kitemeall@gmail.com>
2016-10-20 15:06:28 +0300

    frontend

--- _clean/js/common.js
+++ _clean/js/common.js
@@ -1,70 +1,50 @@
 $(document).ready(function() {
 
-	//Таймер обратного отсчета
-	//Документация: http://keith-wood.name/countdown.html
-	//<div class="countdown" date-time="2015-01-07"></div>
-	var austDay = new Date($(".countdown").attr("date-time"));
-	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+	var pull = $('#pull');
+	menu = $('nav ul');
+	menuHeight = menu.height();
 
-	//Попап менеджер FancyBox
-	//Документация: http://fancybox.net/howto
-	//<a class="fancybox"><img src="image.jpg" /></a>
-	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-	$(".fancybox").fancybox();
-
-	//Навигация по Landing Page
-	//$(".top_mnu") - это верхняя панель со ссылками.
-	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
-
-	//Добавляет классы дочерним блокам .block для анимации
-	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
-
-	//Плавный скролл до блока .div по клику на .scroll
-	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
-		});
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+		console.log(menu)
 	});
 
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
+	
 	//Каруселька
 	//Документация: http://owlgraphic.com/owlcarousel/
-	var owl = $(".carousel");
+	var owl = $("#fb-carousel");
 	owl.owlCarousel({
-		items : 4
+		singleItem:true,
+		slideSpeed : 1000
 	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
-		if (e.deltaY > 0) {
-			owl.trigger("owl.prev");
-		} else {
-			owl.trigger("owl.next");
-		}
-		e.preventDefault();
-	});
-	$(".next_button").click(function(){
+
+	$("#fb-next").click(function(){
 		owl.trigger("owl.next");
 	});
-	$(".prev_button").click(function(){
+	$("#fb-back").click(function(){
 		owl.trigger("owl.prev");
 	});
 
-	//Кнопка "Наверх"
-	//Документация:
-	//http://api.jquery.com/scrolltop/
-	//http://api.jquery.com/animate/
-	$("#top").click(function () {
-		$("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
+	var sis = $("#single-item-slider");
+	sis.owlCarousel({
+		singleItem:true,
+		slideSpeed : 1000
+
+	});
+
+	$("#single-item-next").click(function(){
+		sis.trigger("owl.next");
+	});
+	$("#single-item-back").click(function(){
+		sis.trigger("owl.prev");
 	});
 	
 	//Аякс отправка форм
@@ -82,5 +62,7 @@ $(document).ready(function() {
 		});
 		return false;
 	});
+
+	
 
 });
